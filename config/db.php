2f84c97bd4829755f7cc5b7b77e646ec299154b0
JohnCarlson1 <42758184+JohnCarlson1@users.noreply.github.com>
2019-09-04 15:21:21 -0700

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-  // Create Connection
-  $conn = mysqli_connect("localhost", "root", "Happybird11.", "prodogmom");
-  //check connection
-  if(mysqli_connect_errno()){
-    //connection failed
-    echo 'Failed to connect to MySql '. mysqli_connect_errno();
-  }
-  ?>
